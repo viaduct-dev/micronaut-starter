5d5c3c47c7922e846a01065d289c0f50ca40a1b9
ViaBot <viabot@ductworks.io>
2026-05-06 12:18:58 -0600

    Update from airbnb/viaduct at release/v0.32.0
    GitOrigin-RevId: 0ab6d6a28fe6629b4e92951b1448906015abcf96

--- src/main/kotlin/com/example/viadapp/production/MicronautCodeInjector.kt
+++ src/main/kotlin/com/example/viadapp/production/MicronautCodeInjector.kt
@@ -3,10 +3,10 @@ package com.example.viadapp.production
 import io.micronaut.context.BeanContext
 import jakarta.inject.Singleton
 import javax.inject.Provider
-import viaduct.service.api.spi.TenantCodeInjector
+import viaduct.service.api.spi.CodeInjector
 
 /**
- * Micronaut-based TenantCodeInjector that uses BeanContext for dependency injection.
+ * Micronaut-based CodeInjector that uses BeanContext for dependency injection.
  *
  * This allows resolvers and tenant modules to have their dependencies
  * automatically injected by Micronaut's DI container.
@@ -14,12 +14,19 @@ import viaduct.service.api.spi.TenantCodeInjector
  * This is used in both production and development modes.
  */
 @Singleton
-class MicronautTenantCodeInjector(
+class MicronautCodeInjector(
     private val beanContext: BeanContext
-) : TenantCodeInjector {
+) : CodeInjector {
     override fun <T> getProvider(clazz: Class<T>): Provider<T> {
         return Provider {
             beanContext.getBean(clazz)
         }
+    }
+
+    override fun <T> getProvider(
+        clazz: Class<T>,
+        qualifier: Annotation
+    ): Provider<T> {
+        throw UnsupportedOperationException("MicronautCodeInjector does not support qualified bindings")
     }
 }
