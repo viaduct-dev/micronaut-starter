3d32709b6353a247d8ad6c3fd0a04d285c43122f
ViaBot <viabot@ductworks.io>
2026-05-12 01:56:18 +0000

    Update from airbnb/viaduct at release/v1.0.0
    GitOrigin-RevId: 1bf2348489c1dc486c369617f9ba2b3de9cc6cf6

---
+++ viadapp/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    `java-library`
+    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.viaduct.module)
+}
+
+viaductModule {
+    modulePackageSuffix.set("viadapp")
+}
+
+dependencies {
+    api(libs.viaduct.api)
+    implementation(project(":common"))
+}
